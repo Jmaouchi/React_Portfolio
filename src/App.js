e586bbef4a63082614225b3fe7a701641a06071a
Jugurta Maouchi <djigo.maouchi@yahoo.com>
2022-06-25 21:15:21 -0500

    deploy test

---
+++ src/App.js
@@ -0,0 +1,13 @@
+import logo from './logo.svg';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <h1>Hello world</h1>
+      <p>Setup github pages</p>
+    </div>
+  );
+}
+
+export default App;
